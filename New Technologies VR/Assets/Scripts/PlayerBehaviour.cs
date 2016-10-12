789b3976c241b29eb7f8f4210b2bef78bc5b483c
Jaswir <j.t.raghoe@students.uu.nl>
2016-10-12 18:13:47 +0200

    PlayerBehaviour script added

---
+++ New Technologies VR/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        //Moves Character and also handles input of touchpad to effect movement speed.
+        Vector3 movement = new Vector3(-1, 0, 0);
+        if (Input.touchCount > 0)
+        {
+            float pointer_y = Input.touches[0].deltaPosition.y;
+            movement.x += pointer_y;
+        }
+
+
+
+        _rb.velocity = movement;
+
+    }
+}
